30b9166b529cc14722ad63c935850a3fb99e2cc4
blackcagouprod <blackcagou@outlook.fr>
2024-10-23 21:55:15 +0200

    fix dark-mode bug

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.classList.add('dark-mode');
         document.querySelector('.sidebar').classList.add('dark-mode');
         document.querySelectorAll('.middleContent').forEach(el => el.classList.add('dark-mode'));
+        document.querySelectorAll('.middle').forEach(el => el.classList.add('dark-mode'));
         document.querySelector('footer').classList.add('dark-mode');
+        document.querySelectorAll(' h4').forEach(el => el.classList.add('dark-mode')); // Ajout pour les h4
+        document.querySelector('iframe').classList.toggle('dark-mode');
     }
 
     // Ajouter un écouteur d'événement au bouton de bascule de thème
@@ -14,7 +17,10 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.classList.toggle('dark-mode');
         document.querySelector('.sidebar').classList.toggle('dark-mode');
         document.querySelectorAll('.middleContent').forEach(el => el.classList.toggle('dark-mode'));
+        document.querySelectorAll('.middle').forEach(el => el.classList.add('dark-mode'));
         document.querySelector('footer').classList.toggle('dark-mode');
+        document.querySelectorAll('.sph h4').forEach(el => el.classList.toggle('dark-mode')); // Ajout pour les h4
+        document.querySelector('iframe').classList.toggle('dark-mode');
 
         // Sauvegarder le choix de l'utilisateur dans le localStorage
         if (document.body.classList.contains('dark-mode')) {
@@ -23,4 +29,6 @@ document.addEventListener('DOMContentLoaded', function() {
             localStorage.setItem('theme', 'light');
         }
     });
+console.log(currentTheme);
+    
 });
